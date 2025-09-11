e47fc44a16b4516ed71e1c30879c6c0b2daa4fc2
joyria <jowairiaibrahim83@gmail.com>
2025-09-11 17:31:56 +0200

    test 2-2mjs

--- module-2/task_2-2/task_2-2.mjs
+++ module-2/task_2-2/task_2-2.mjs
@@ -4,27 +4,51 @@ import { printOut, newLine } from "../../common/script/utils.mjs";
 
 printOut("--- Part 1 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+const sum= 2 + 3 * (2 - 4) * 6 //-34
+
+printOut("svar:",sum);
 printOut(newLine);
 
 printOut("--- Part 2 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+let fulltall = 25340 // mm
+const inch = 25.4 // mm
+let sum= inch / fulltall // 977,64
+
+printOut(" sum ");
 printOut(newLine);
 
 printOut("--- Part 3 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+let days = 3;
+let hours = 12;
+let minuttes = 14;
+let seconds = 45;
+const sum = (days * 24 * 60)+ (hours * 60) + minuttes +(seconds / 60);
+
+printOut(minuttes);
 printOut(newLine);
 
 printOut("--- Part 4 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+let minuttes = 6322.52;
+let seconds = minuttes * 60;
+let hours = minuttes / 60;
+let days = hours / 24;
+
+printOut( days, hours,  minuttes, seconds);
 printOut(newLine);
 
 printOut("--- Part 5 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+const dollar = 54;
+const exgangeRate= 76/8.6
+const nok= dollar*exgangeRate//54*(76/8.6)
+
+printOut(nok);
 printOut(newLine);
 
 printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
@@ -34,18 +58,35 @@ printOut(newLine);
 
 printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
+firststats = 5 > 3
+secondstats = 7 >= 7
+thirdstats = "a" > "b"
+fourthstats= 1 >= "a"
+fivthstats = "2500" > "abcd"
+sixthstats = "arne" !== "thomas"
+seventhstats = 2 != 5
+eathstats = "abcd" > "bcd"
+
 printOut("Replace this with you answer!");
 printOut(newLine);
 
 printOut("--- Part 8 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
+let printenumberone = "254" 
+let printnumbertwo = "57.23" 
+let printnumberthree = "25 kroner"
+
+let printenumberone = Number(printnumberone)
+let printnumbertwo = Number(printnumbertwo)
+let printnumberthree = Number(printnumberthree)
 printOut("Replace this with you answer!");
 printOut(newLine);
 
 printOut("--- Part 9 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
 printOut("Replace this with you answer!");
-printOut(newLine);
+printOut(newLine); 
+
 
 /* Task 10*/
 printOut("--- Part 10 ---------------------------------------------------------------------------------------------");
